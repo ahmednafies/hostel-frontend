2f31854af12596921d1463602e56a829e2582f96
ahmednafies <ahmed.nafies@gmail.com>
2019-06-15 18:25:35 +0300

    firebase config

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAjjuOzY37rmGkO-NxsxWDZEwfDtgrO7Mg",
+  authDomain: "port-hostel.firebaseapp.com",
+  databaseURL: "https://port-hostel.firebaseio.com",
+  projectId: "port-hostel",
+  storageBucket: "port-hostel.appspot.com",
+  messagingSenderId: "379551629738",
+  appId: "1:379551629738:web:6247847f77627dbf",
+}
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
+
+export default firebase
