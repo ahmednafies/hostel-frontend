143ff7b2d9b9e5b20bd892d769812faebb999d92
ahmednafies <ahmed.nafies@gmail.com>
2019-07-06 21:54:16 +0300

    image card for rooms is added

---
+++ src/Components/MediaCard/MediaCard.js
@@ -0,0 +1,57 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Typography from "@material-ui/core/Typography";
+import blue from "@material-ui/core/colors/blue";
+
+const useStyles = makeStyles({
+  card: {
+    maxWidth: 350,
+    marginTop: 30
+  },
+  media: {
+    height: 0,
+    paddingTop: "56.25%" // 16:9
+  },
+  actions: {
+    display: "flex"
+  },
+
+  expandOpen: {
+    transform: "rotate(180deg)"
+  },
+  avatar: {
+    backgroundColor: blue[800]
+  }
+});
+
+function MediaCard(props) {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.card}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image={props.image.src}
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {props.image.name}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+}
+
+MediaCard.propTypes = {
+  image: PropTypes.object.isRequired
+};
+
+export default MediaCard;
--- src/Pages/Rooms/Rooms.js
+++ src/Pages/Rooms/Rooms.js
@@ -1,13 +1,83 @@
-import React, { Component } from "react";
+import React from "react";
+import { withStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import Slide from "@material-ui/core/Slide";
+import { MediaCard } from "../../Components/MediaCard";
+import { bathrooms } from "../../Components/Album/Photos/Bathrooms";
+import { doublerooms } from "../../Components/Album/Photos/DoubleRooms";
+import { twinbedrooms } from "../../Components/Album/Photos/TwinBedrooms";
+import { singlerooms } from "../../Components/Album/Photos/SingleRooms";
+import { triplerooms } from "../../Components/Album/Photos/TripleRooms";
 
-class Rooms extends Component {
-  render() {
-    return (
-      <div>
-        <h2>Rooms</h2>
-      </div>
-    );
+const rooms = [
+  {
+    src: singlerooms[0].src,
+    name: "Single Rooms"
+  },
+  {
+    src: doublerooms[0].src,
+    name: "Double Rooms"
+  },
+  {
+    src: twinbedrooms[0].src,
+    name: "Twin Bedrooms"
+  },
+  {
+    src: triplerooms[0].src,
+    name: "Triple Rooms"
+  },
+  {
+    src: bathrooms[0].src,
+    name: "Bathrooms"
   }
+];
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1
+  },
+  paper: {
+    padding: theme.spacing.unit * 2,
+    textAlign: "center",
+    color: theme.palette.text.secondary
+  },
+  grid: {
+    alignItems: "center",
+    textAlign: "center",
+    justify: "center"
+  }
+});
+
+function Rooms(props) {
+  const { classes } = props;
+
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={24}>
+        <Grid item xs={12} className={classes.grid}>
+          <div>
+            <h1>Our Rooms</h1>
+          </div>
+        </Grid>
+        {rooms.map((room, index) => {
+          return (
+            <Slide
+              direction="left"
+              in={true}
+              key={index}
+              style={{ transitionDelay: index * 50 }}
+              mountOnEnter
+              unmountOnExit
+            >
+              <Grid item xs={4}>
+                <MediaCard image={room} />
+              </Grid>
+            </Slide>
+          );
+        })}
+      </Grid>
+    </div>
+  );
 }
 
-export default Rooms;
+export default withStyles(styles)(Rooms);
