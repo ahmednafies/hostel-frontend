86eba23fae6a31b1a9672fac67f5da05995587bf
ahmednafies <ahmed.nafies@gmail.com>
2019-07-07 13:02:51 +0300

    contacts added

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -1,13 +1,60 @@
-import React, { Component } from "react";
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import ContactEmailIcon from "@material-ui/icons/ContactMailRounded";
+import PhoneIcon from "@material-ui/icons/PhoneRounded";
+import WebIcon from "@material-ui/icons/WebRounded";
+import Avatar from "@material-ui/core/Avatar";
 
-class Contact extends Component {
-  render() {
-    return (
-      <div>
-        <h2>Contact</h2>
-      </div>
-    );
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: "80%",
+    marginTop: theme.spacing(3),
+    overflowX: "auto"
+  },
+  table: {
+    minWidth: 650
   }
+}));
+
+function createData(name, value, icon) {
+  return { name, value, icon };
 }
 
+const rows = [
+  createData("Email", "info@harbourhostel.ee", <ContactEmailIcon />),
+  createData("Website", "  https://harbourhostel.ee", <WebIcon />),
+  createData("Phone No", "666 0045", <PhoneIcon />)
+];
+
+function Contact() {
+  const classes = useStyles();
+
+  return (
+    <Paper className={classes.root}>
+      <Typography>
+        <h1>Contact</h1>
+      </Typography>
+      <Table className={classes.table}>
+        <TableBody>
+          {rows.map(row => (
+            <TableRow key={row.name}>
+              <TableCell component="th" scope="row">
+                <Avatar>{row.icon}</Avatar>
+              </TableCell>
+              <TableCell align="left">
+                <Typography>{row.value}</Typography>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </Paper>
+  );
+}
 export default Contact;
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,8 +1,24 @@
 import React from "react";
 import { SlideShow } from "../../Components/Album";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: "100%",
+    marginTop: theme.spacing(3),
+    overflowX: "auto"
+  }
+}));
 
 function Home() {
-  return <SlideShow />;
+  const classes = useStyles();
+
+  return (
+    <Paper className={classes.root}>
+      <SlideShow />
+    </Paper>
+  );
 }
 
 export default Home;
