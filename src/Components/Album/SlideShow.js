94bf6407d24fe282b32f3115a987257620f6e1ab
ahmednafies <ahmed.nafies@gmail.com>
2019-07-12 21:46:08 +0300

    hooks

--- src/Components/Album/SlideShow.js
+++ src/Components/Album/SlideShow.js
@@ -1,19 +1,16 @@
-import React, { Component } from "react";
+import React from "react";
 import BackgroundSlider from "react-background-slider";
 import { carousel } from "./Photos/Carousel";
-
-class SlideShow extends Component {
-  render() {
-    return (
-      <div>
-        <BackgroundSlider
-          images={carousel.map(photo => photo.src)}
-          duration={5}
-          transition={2}
-        />
-      </div>
-    );
-  }
+function SlideShow() {
+  return (
+    <div>
+      <BackgroundSlider
+        images={carousel.map(photo => photo.src)}
+        duration={5}
+        transition={2}
+      />
+    </div>
+  );
 }
 
 export default SlideShow;
